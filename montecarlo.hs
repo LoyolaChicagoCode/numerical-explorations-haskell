0201fe5cf9bdb4fa08e3afd5a02d710a8997dab7
Konstantin Laufer <laufer@cs.luc.edu>
2014-02-16 23:05:30 -0600

    minor cosmetic improvement

--- montecarlo.hs
+++ montecarlo.hs
@@ -6,7 +6,7 @@ import Data.List
 inCircle (x, y) = sqr x + sqr y <= 1 where sqr x = x * x
 
 monteCarloCircleArea :: Int64 -> [(Double, Double)] -> Double
-monteCarloCircleArea num darts = 
+monteCarloCircleArea    num      darts = 
   4 * (fromIntegral (length $ filter inCircle $ genericTake num darts)) / (fromIntegral num)
 
 toPairs (x1 : x2 : xs) = (x1, x2) : toPairs xs
