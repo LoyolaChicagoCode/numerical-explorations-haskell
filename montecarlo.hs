318ced216d2fd6590eacfb3a30a8563a479bf0b4
Konstantin Laufer <laufer@cs.luc.edu>
2014-02-16 22:37:56 -0600

    flattened structure, added beginnings of a Monte Carlo example

---
+++ montecarlo.hs
@@ -0,0 +1,24 @@
+import System.Environment (getArgs)
+import System.Random
+import Data.Int
+import Data.List
+
+sqr x = x * x
+
+inCircle (x, y) = sqr x + sqr y <= 1
+
+toPairs []             = []
+toPairs (x1 : x2 : xs) = (x1, x2) : toPairs xs
+
+monteCarloCircleArea :: Int64 -> [(Double, Double)] -> Double
+monteCarloCircleArea num darts = 
+  4 * (fromIntegral (length $ filter inCircle $ genericTake num darts)) / (fromIntegral num)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let [num] = map (read :: String -> Int64) args
+  g <- getStdGen
+  let pi = monteCarloCircleArea num $ toPairs $ (randoms g :: [Double])
+  putStrLn $ "pi = " ++ show pi ++ " for " ++ show num ++ " darts"
+
